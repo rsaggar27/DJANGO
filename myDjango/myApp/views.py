f4baab72430d76e59a4d565961b88fb1ec651fed
rsaggar27 <rishabhsaggar30@gmail.com>
2025-03-30 15:17:42 +0530

    app made

---
+++ myDjango/myApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def first_page(request):
+    return render(request,'myApp/app-first.html')
+
+
+
+
+
