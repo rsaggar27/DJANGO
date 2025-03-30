f930822d88ad578a0c39c9d5e80896584f7b9c4c
rsaggar27 <rishabhsaggar30@gmail.com>
2025-03-30 15:43:25 +0530

    tailwind connected

--- myDjango/myDjango/settings.py
+++ myDjango/myDjango/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myApp'
+    'myApp',
+    'tailwind',
+    'theme'
 ]
+
+TAILWIND_APP_NAME='theme'
+INTERNAL_IPS=['127.0.0.1']
+NPM_BIN_PATH = "/usr/local/bin/npm"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware'
 ]
 
 ROOT_URLCONF = 'myDjango.urls'
